7db222124584cf1859ca8c6486cdb289caa17207
Cerecero <ale.rey.cer@outlook.com>
2023-03-06 19:28:03 -0600

    added a button

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ class gui{
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        JButton button1 = new JButton("Press");
+        frame.getContentPane().add(button1);
         frame.setVisible(true);
     }
 }
