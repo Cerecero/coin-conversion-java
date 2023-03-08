f015f214276596c25749d008c6780c0724f0a503
Cerecero <ale.rey.cer@outlook.com>
2023-03-07 20:31:13 -0600

    added Jtextfield to the mainframe

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,15 @@ class gui{
         JFrame frame = new JFrame("Alura Challenge");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,400);
-        JButton button1 = new JButton("Press");
-        frame.getContentPane().add(button1);
-        frame.setVisible(true);
+        //JButton button1 = new JButton("Press");
+        //frame.getContentPane().add(button1);
+        //frame.setVisible(true);
+        Text test = new Text();
+        frame.add(test);
 
-        new MyDropdownMenu();
+
 
 
     }
 
-    public static class MyDropdownMenu extends JFrame {
-        public MyDropdownMenu() {
-            // Create an array of items to be displayed in the dropdown menu
-            String[] items = {"Conversion de monedas", "COnversion de temperatura"};
-
-            // Create a new JComboBox with the items array as its argument
-            JComboBox<String> dropdown = new JComboBox<>(items);
-
-            // Add the dropdown to the JFrame
-            add(dropdown);
-
-            // Set the size of the JFrame and make it visible
-            setSize(300, 200);
-            setVisible(true);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        }
-}
 }
