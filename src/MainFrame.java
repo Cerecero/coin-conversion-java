750538dd966ffe2eef092cb6315da8e00483f37b
Cerecero <ale.rey.cer@outlook.com>
2023-03-08 19:03:51 -0600

    moved the code from Main to MainFrame to handle the creation of the frame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,13 +4,15 @@ import java.awt.*;
 public class MainFrame extends JFrame {
 
     MainFrame(){
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,400);
-        frame.setResizable(false);
+        this.setTitle("Alura Challenge");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300,400);
+        this.setResizable(false);
         //JButton button1 = new JButton("Press"); //frame.getContentPane().add(button1);
 
         ImageIcon image = new ImageIcon("alura.svg");
-        frame.getContentPane().setBackground(new Color( 0xD8DFE8));
-        frame.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color( 0xD8DFE8));
+        this.setIconImage(image.getImage());
+        this.setVisible(true);
     }
 }
