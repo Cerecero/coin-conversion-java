1d10197034defc7a0ff57dec6c5c39e2f26805d2
Cerecero <ale.rey.cer@outlook.com>
2023-03-08 18:57:36 -0600

    added a constructor to the MainFrame class

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,16 @@
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    MainFrame(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,400);
+        frame.setResizable(false);
+        //JButton button1 = new JButton("Press"); //frame.getContentPane().add(button1);
+
+        ImageIcon image = new ImageIcon("alura.svg");
+        frame.getContentPane().setBackground(new Color( 0xD8DFE8));
+        frame.setIconImage(image.getImage());
+    }
 }
