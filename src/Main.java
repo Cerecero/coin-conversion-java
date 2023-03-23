5e928289e51882a8c616a29a3496b8cc15b1e6c7
Cerecero <ale.rey.cer@outlook.com>
2023-03-22 19:10:20 -0600

    removed the commits

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,10 @@ import java.awt.*;
 
 class gui{
     public static void main(String args[]){
-        //JFrame frame = new JFrame("Alura Challenge");
-
-        //JLabel label = new JLabel();
-        //label.setText("Alura Challenge Test");
-        //label.setHorizontalAlignment(JLabel.CENTER);
-        //frame.setVisible(true);
+        
         MainFrame frame = new MainFrame();
-        //new MainFrame();
+
         frame.setVisible(true);
-        //frame.add(label);
+
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,31 +6,23 @@ public class MainFrame extends JFrame {
     MainFrame(){
         setTitle("Alura Challenge");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,200);
+        setSize(400,200);
         setResizable(false);
 
         setLayout(null);
-        //JButton button1 = new JButton("Press"); //frame.getContentPane().add(button1);
 
         JLabel label = new JLabel("Seleccione una opcion de conversion:");
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setBounds(15,20,300,40);
         add(label);
 
-        //ImageIcon image = new ImageIcon("alura.svg");
         getContentPane().setBackground(new Color( 0xD8DFE8));
-        //this.setIconImage(image.getImage());
 
         /*CREATES A DROPDOWN MENU*/
         String[] options = {"Conversion de monedas", "Conversion de Temperatura"};
         JComboBox<String> dropdown = new JComboBox<>(options);
         dropdown.setBounds(50,50,200,30);
         add(dropdown);
-
-        //JTextField inputField = new JTextField();
-        //inputField.setBounds(50,50,150,30);
-
-        //add(inputField);
 
         setVisible(true);
     }
