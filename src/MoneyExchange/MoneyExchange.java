7c09be0b340e02b38c793487e094565682939f2b
Cerecero <ale.rey.cer@outlook.com>
2023-03-23 19:32:03 -0600

    created the logic to handle pesos to usd

--- src/MoneyExchange/MoneyExchange.java
+++ src/MoneyExchange/MoneyExchange.java
@@ -2,9 +2,12 @@ package MoneyExchange;
 
 public class MoneyExchange {
 
-    public void ExchangePesoToDollars(double input) {
-        double dolarAmout = input / 0.054;
-        dolarAmout
+    public double ExchangeDollarsToPeso(double input) { /*Make it return void...maybe*/
+        double exchangeRateUSD = 0.054;
+        double dolarAmount = input * exchangeRateUSD;
+        dolarAmount = (double) Math.round(dolarAmount * 100d)/100;
+        return dolarAmount;
+        /*Here goes the message to show the user the result*/
     }
 
 }
