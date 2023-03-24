82375c32289e5954e27d1541e9a702f3b1956141
Cerecero <ale.rey.cer@outlook.com>
2023-03-23 18:58:00 -0600

    created packages

---
+++ src/MoneyExchange/MoneyExchange.java
@@ -0,0 +1,10 @@
+package MoneyExchange;
+
+public class MoneyExchange {
+
+    public void ExchangePesoToDollars(double input) {
+        double dolarAmout = input / 0.054;
+        dolarAmout
+    }
+
+}
--- src/Temperature.java
+++ src/Temperature.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class Temperature {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the money you wish to convert: ");
-        String money = input.nextLine();
-        System.out.println("this is your money"+money);
 
-
-    }
 
 }
