06e1c5483686569676e0acdc4827a9104c119e6f
Cerecero <ale.rey.cer@outlook.com>
2023-03-23 19:44:45 -0600

    created ForeignExchangeToLocal.java

--- src/MoneyExchange/ForeignExchangeToLocal.java
+++ src/MoneyExchange/ForeignExchangeToLocal.java
@@ -1,13 +1,11 @@
 package MoneyExchange;
 
-public class MoneyExchange {
-
-    public double ExchangeDollarsToPeso(double input) { /*Make it return void...maybe*/
+public class ForeignExchangeToLocal {
+    public double ExchangePesoToDollars(double input) { /*Make it return void...maybe*/
         double exchangeRateUSD = 0.054;
-        double dolarAmount = input / exchangeRateUSD;
+        double dolarAmount = input * exchangeRateUSD;
         dolarAmount = (double) Math.round(dolarAmount * 100d)/100;
         return dolarAmount;
         /*Here goes the message to show the user the result*/
     }
-
 }
