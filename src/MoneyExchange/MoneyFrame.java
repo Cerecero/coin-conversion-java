a262f657b90320aa92b0f7ea3cf22bce43930f66
Cerecero <ale.rey.cer@outlook.com>
2023-04-05 17:39:24 -0600

    removed the previous code, and started new

--- src/MoneyExchange/MoneyFrame.java
+++ src/MoneyExchange/MoneyFrame.java
@@ -6,20 +6,34 @@ import java.text.NumberFormat;
 
 public class MoneyFrame extends JFrame {
     public MoneyFrame(){
-        setTitle("Conversion de Monedas");
+        setTitle("Cambio de moneda.");
+        setSize(500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400,200);
-        setResizable(false);
-
-        JPanel panel = new JPanel(new GridLayout(2,3));
-        add(panel);
-
-        NumberFormat input = NumberFormat.getNumberInstance();
-        JFormattedTextField textField = new JFormattedTextField(input);
-
-        panel.add(textField);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         setVisible(true);
 
     }
 }
+/*public MoneyFrame(){
+        setTitle("Conversion de Monedas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,200);
+        setResizable(false);
+        setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+
+        JPanel panel = new JPanel();
+        //panel.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+        add(panel);
+
+        NumberFormat input = NumberFormat.getNumberInstance();
+        JFormattedTextField textField = new JFormattedTextField(input);
+
+        textField.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+
+        panel.add(textField);
+
+        setVisible(true);
+
+    }*/
