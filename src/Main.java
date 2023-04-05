474bcec9f6df5d5812acdcf271e72b895a0c37c5
Cerecero <ale.rey.cer@outlook.com>
2023-04-05 17:38:41 -0600

    changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-
+import MoneyExchange.MoneyFrame;
 class gui{
     public static void main(String args[]){
 
-        MainFrame frame = new MainFrame();
-
+        //MainFrame frame = new MainFrame();
+        MoneyFrame money = new MoneyFrame();
     }
 }
