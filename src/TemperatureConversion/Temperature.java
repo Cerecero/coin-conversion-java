eeb2985ad52b303ea8bf0e1f7ed5e52557c24977
Cerecero <ale.rey.cer@outlook.com>
2023-04-10 15:47:01 -0600

    created a hashmap to hold the values

--- src/TemperatureConversion/Temperature.java
+++ src/TemperatureConversion/Temperature.java
@@ -1,9 +1,19 @@
 package TemperatureConversion;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Temperature {
-
+//farenheit F, Celsisu C, Kelvin K
+    public static Map<String, Double> InputToKelandFar(double input){
+        Map<String, Double> result = new HashMap<>();
+        double KELVIN = input + 273.15;
+        double FARENHEIT = ((9/5) * input) + 32;
+        result.put("Kel", KELVIN);
+        result.put("Far", FARENHEIT);
+        return result;
+    }
 
 
 }
