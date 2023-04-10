cbc5bcf31e83f0699482178c634af66a14337591
Cerecero <ale.rey.cer@outlook.com>
2023-04-10 13:54:19 -0600

    renamed the method TemperatureConversion to TemperatureFrame

--- src/TemperatureConversion/TemperatureFrame.java
+++ src/TemperatureConversion/TemperatureFrame.java
@@ -1,4 +1,18 @@
 package TemperatureConversion;
 
-public class TemperatureFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class TemperatureFrame extends JFrame {
+    public TemperatureFrame() {
+        setTitle("Cambio de Temperatura");
+        setSize(500,500);
+        setLayout(new GridLayout(3 , 1 , 0, 0));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+
+
+        setVisible(true);
+
+    }
 }
