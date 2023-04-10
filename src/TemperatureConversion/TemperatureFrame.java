9570405edcecc62154beacf53e3f7a0309d980b8
Cerecero <ale.rey.cer@outlook.com>
2023-04-10 15:31:45 -0600

    added a keylistener to the input celsius

--- src/TemperatureConversion/TemperatureFrame.java
+++ src/TemperatureConversion/TemperatureFrame.java
@@ -1,7 +1,12 @@
 package TemperatureConversion;
 
 import javax.swing.*;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TemperatureFrame extends JFrame {
     public TemperatureFrame() {
@@ -9,6 +14,28 @@ public class TemperatureFrame extends JFrame {
         setSize(500,500);
         setLayout(new GridLayout(3 , 1 , 0, 0));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JTextField inputCelsius = new JTextField();
+        JTextField inputFarenheit = new JTextField();
+        JTextField inputKelvin = new JTextField();
+
+        panel.add(inputCelsius);
+        panel.add(inputFarenheit);
+        panel.add(inputKelvin);
+
+        inputCelsius.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e){
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    inputKelvin.setText("test");
+                }
+            }
+        });
+
+        add(panel, BorderLayout.NORTH);
 
         setVisible(true);
 
