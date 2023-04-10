10ba67339df62178e414e1f8521868dc12f61028
Cerecero <ale.rey.cer@outlook.com>
2023-04-10 13:37:13 -0600

    removed comments

--- src/MoneyExchange/MoneyFrame.java
+++ src/MoneyExchange/MoneyFrame.java
@@ -143,25 +143,3 @@ public class MoneyFrame extends JFrame {
     }
 
 }
-
-/*public MoneyFrame(){
-        setTitle("Conversion de Monedas");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400,200);
-        setResizable(false);
-        setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
-
-        JPanel panel = new JPanel();
-        //panel.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
-        add(panel);
-
-        NumberFormat input = NumberFormat.getNumberInstance();
-        JFormattedTextField textField = new JFormattedTextField(input);
-
-        textField.setAlignmentX(Component.BOTTOM_ALIGNMENT);
-
-        panel.add(textField);
-
-        setVisible(true);
-
-    }*/
